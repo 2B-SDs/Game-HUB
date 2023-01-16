4eb3334f883dd0ea526001d11c3122bcd09b9c1e
2B-SDs <122784859+2B-SDs@users.noreply.github.com>
2023-01-16 15:49:16 +0545

    Add files via upload

---
+++ Congrats.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+/**
+ * @brief This file congratulates the user.
+ * After wining all the round
+ * @file Congrats.h
+ * @see congrats()
+ *
+ */
+int congrats()
+{
+    system("color A");
+    printf("\n ");
+    printf("\n ");
+    printf("\n ");
+    printf("\n ");
+    printf("\n ");
+    printf("\n ");
+    Sleep(700);
+    system("color A");
+    printf("\033[1;32m");
+    ///@brief change text color to green
+    printf("\033[1;37m");
+    ///@brief change text size to bigger
+    system("color A");
+    printf("\t\t\t&&&&&&&& &&&&&& &&     && &&&&&&&&   &&&&&&   &&&&&&& &&&&&& &&  && &&     &&&&&&& &&&&&& &&&&&& &&&&&& &&&&&& &&      &&   &\n");
+    printf("\t\t\t&&       &&  && && &   && &&         &&  &&   &&   &&   &&   &&  && &&     &&   &&   &&   &&  &&   &&   &&  && && &    &&   &  \n");
+    printf("\t\t\t&&       &&  && &&   & && &&   &&&&& &&&&&&   &&&&&&&   &&   &&  && &&     &&&&&&&   &&   &&  &&   &&   &&  && &&   &  &&   &     \n");
+    printf("\t\t\t&&       &&  && &&     && &&   && && &&   &&  &&   &&   &&   &&  && &&     &&   &&   &&   &&  &&   &&   &&  && &&      &&   &     \n");
+    printf("\t\t\t&&&&&&&& &&&&&& &&     && &&&&&&& && &&    && &&   &&   &&   &&&&&& &&&&&& &&   &&   &&   &&&&&& &&&&&& &&&&&& &&      &&   O \n\n");
+    printf("\t\t******************************************************************************************************************************************\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
+    printf("\n");
+    printf("\033[0m");
+    ///@brief reset text format
+    system("color 2");
+    printf("\t\t\tCongratulations!!, the adventure Hero on completing the journey of Adventurous World!\n");
+    Sleep(700);
+    printf("\t\t\tyou have faced many challenges and obstacles throughout this journey, but you have proven to be a skilled and determined player.\n ");
+    Sleep(700);
+    printf("\t\t\tYour success in this game is a reflection of your strength and resilience in the face of adversity.\n");
+    Sleep(700);
+    printf("\t\t\tRemember that life is full of ups and downs, but with perseverance and determination, you can overcome any obstacle and achieve your goals \n");
+    Sleep(700);
+    printf("\t\t\tWe hope that this game has inspired you to believe in yourself and to embrace the challenges that come your way. \n \n \n");
+    Sleep(700);
+    printf("\t\t\tThank you for playing Adventurous World, and we hope you have had a fun and rewarding experience.\n\n\n");
+    Sleep(700);
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include "menu.h"
+#include <windows.h>
+#include "AdvWorld.h"
+
+///@brief top margin...
+void topspace()
+{
+	printf("\n\n\n");
+	printf("\n\n\n");
+	printf("\n\n\n");
+	printf("\n\n\n");
+}
+
+/**
+ * @mainpage
+ * @file main.c
+ * @brief This function print the Name of the project
+ * @author Sujan Tamang, Binaya Karki, Bishal Karki, Devendra Khatri .
+ * @details And calls menu function
+ * @file menu.h
+ */
+int main()
+{
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&\n");
+	printf("\t\t\t     &&           \n");
+	printf("\t\t\t     &&   &&&&&&&&\n");
+	printf("\t\t\t     &&   &&&&&  &\n");
+	printf("\t\t\t     &&      &&  &\n");
+	printf("\t\t\t     &&      &&  &\n");
+	printf("\t\t\t     &&&&&&&&&&  &\n");
+	Sleep(700);
+	system("cls");
+
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&      \n");
+	printf("\t\t\t     &&                  &&   &&     \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&    \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&   \n");
+	printf("\t\t\t     &&      &&  &    &&         &&  \n");
+	printf("\t\t\t     &&      &&  &   &&           && \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&\n");
+	Sleep(700);
+	system("cls");
+
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&& \n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  && \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  && \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   && \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          && \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          && \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          && \n");
+	Sleep(700);
+	system("cls");
+
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&&  &&&&&&&&&&\n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   &&  &&&&&&&&& \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          &&  &&        \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          &&  &&        \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          &&  &&&&&&&&&&\n");
+	Sleep(700);
+	printf("\n\n");
+	printf("\n\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&&&&&&&&&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&\n");
+
+	Sleep(700);
+	system("cls");
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&&  &&&&&&&&&&\n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   &&  &&&&&&&&& \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          &&  &&        \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          &&  &&        \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          &&  &&&&&&&&&&\n");
+	printf("\n\n");
+	printf("\n\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&&&&&&&&&   &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&&&&&&&&&\n");
+
+	Sleep(700);
+	system("cls");
+	topspace();
+	printf("\t\t\t     &&&&&&&&&&&&&        &&&&&        &&&&      &&&&  &&&&&&&&&&\n");
+	printf("\t\t\t     &&                  &&   &&       && &     &  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&&&&      &&     &&      &&  &&  &&  &&  &&        \n");
+	printf("\t\t\t     &&   &&&&&  &     &&&&&&&&&&&     &&   &&&&   &&  &&&&&&&&& \n");
+	printf("\t\t\t     &&      &&  &    &&         &&    &&          &&  &&        \n");
+	printf("\t\t\t     &&      &&  &   &&           &&   &&          &&  &&        \n");
+	printf("\t\t\t     &&&&&&&&&&  &  &&             &&  &&          &&  &&&&&&&&&&\n");
+	printf("\n\n");
+	printf("\n\n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&&&&&&&&& \n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+	printf("\t\t\t\t\t\t\t\t\t         &&&&&&&&&&   &&      &&   &&&&&&&&&  \n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&      &&   &&       & \n");
+	printf("\t\t\t\t\t\t\t\t\t         &&      &&   &&&&&&&&&&   &&&&&&&&&& \n");
+	Sleep(1500);
+	system("cls");
+	/**
+	 * @brief function menu from menu.h header file
+	 * @file menu.h
+	 *
+	 * */
+	///@brief calling menu Function after the intro or the name of the project
+	menu();
+	return 0;
+}
